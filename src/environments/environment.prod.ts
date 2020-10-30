94abff52e2a2a96183415209dfb61ffc136405c8
Manoj <gmanoj.isaac@gmail.com>
2020-10-30 16:17:10 +0530

    Firebase completed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    authDomain: "jeniapp-f7c47.firebaseapp.com",
+    databaseURL: "https://jeniapp-f7c47.firebaseio.com",
+    projectId: "jeniapp-f7c47",
+    storageBucket: "jeniapp-f7c47.appspot.com",
+    messagingSenderId: "207310949849",
+    appId: "1:207310949849:web:c1fb89a692357f62ef5dc8"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    authDomain: "jeniapp-f7c47.firebaseapp.com",
+    databaseURL: "https://jeniapp-f7c47.firebaseio.com",
+    projectId: "jeniapp-f7c47",
+    storageBucket: "jeniapp-f7c47.appspot.com",
+    messagingSenderId: "207310949849",
+    appId: "1:207310949849:web:c1fb89a692357f62ef5dc8"
+  }
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
